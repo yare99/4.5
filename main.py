3583b37384fad2218758b0ccfa1134a8fe13e943
yare99 <134287340+yare99@users.noreply.github.com>
2023-05-24 09:21:09 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+equip = ['Jugador 1', 'Jugador 2', 'Jugador 3', 'Jugador 4', 'Jugador 5']
+
+equip[0]= 'Mbappe'
+equip[1]='Benzema'
+equip[2]='Ronaldo'
+equip[3]='Messi'
+equip[4]='Neymar'
+
+print(equip)
+
+num=1
+for jugador in equip:
+  print(f'El jugador {num} es {jugador}')
+  num+=1
